e515f5279cf385a7a4b7be5505093bd2381c67b5
Ruo Chen <chenruo@ufl.edu>
2025-03-21 18:28:38 -0400

    Meh

--- include/auv.h
+++ include/auv.h
@@ -1,12 +1,33 @@
 // This is going to be a very simplified AUV model in order to represent
 // "exploring" the caves
 
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <stdexcept>
+#include <vector>
+#include "../include/dag.h"
+
 class AUV {
     private:
+        bool is_leader;
         int hierarchy;
         int id;
 
+        std::set<Node*> explored_nodes;
+        std::set<Node*> next_nodes;
+        // Nodes designated for communication purposes
+        std::set<Node*> comm_nodes;
+
+        std::set<AUV*> group;
+
     public:
         int test;
+
+        // Define behaviors here
+        void mergeGroups();
+        void getNextNode();
+        void chooseNode();
         
 };
